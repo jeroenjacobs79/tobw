079442df5a1660f94de81a059373ff220d6bcd33
Jeroen Jacobs <jeroen.jacobs@headincloud.be>
2019-11-24 18:34:49 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,98 @@
+// Tale of the Black Wyvern (TOBW)
+// A tribute to Legend of the Red Dragon (LORD)
+// (c) Jeroen Jacobs, 2019
+
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	//	"golang.org/x/text/encoding/charmap"
+	"io"
+	"net"
+	"tobw/telnet"
+)
+
+const (
+	APP_NAME = "Tale of the Black Wyvern"
+	APP_CODE = "TOBW"
+	TELNET_PORT = 5000
+)
+
+func main() {
+	// set log format to include timestamp, even when TTY is attached.
+	log.SetFormatter(&log.TextFormatter{
+		FullTimestamp:true},
+	)
+	// set log level
+	log.SetLevel(log.TraceLevel)
+	// startup message
+	log.Infof("%s (%s) is starting up...\n", APP_NAME, APP_CODE)
+
+	// codepage test stuff that needs to be removed
+	// testb := byte(176)
+	// resb := charmap.CodePage437.DecodeByte(testb)
+	// charmap.CodePage437.NewDecoder(). (need to check this)
+	//fmt.Println(string(resb))
+
+	// start telnet listener
+	log.Infof("Starting telnet listener on port %d...\n", TELNET_PORT)
+    srv, err := net.Listen("tcp", "127.0.0.1:5000")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// close listener on exit
+	defer func () {
+		err := srv.Close()
+		if err!=nil {
+			log.Errorln(err.Error())
+		}
+	}()
+
+    // start accepting connections
+	log.Infof("Telnet listener started.")
+	for {
+		// Listen for an incoming connection.
+		conn, err := srv.Accept()
+		if err != nil {
+			log.Errorln(err.Error())
+		}
+		// Handle connections in a new goroutine.
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// Make a buffer to hold incoming data.
+	buf := make([]byte, 1024)
+	telnetConn := telnet.NewConnection(conn)
+	log.Infof("%s - Connected\n", telnetConn.RemoteAddr())
+	active := true
+
+	// Close the connection when you're done with it.
+	defer func () {
+		log.Infof("%s - Disconnected\n", telnetConn.RemoteAddr())
+		err := conn.Close()
+		if err!=nil {
+			log.Error(err.Error())
+		}
+	}()
+
+	// while connection is active, process event loop
+	for active {
+		// Read the incoming connection into the buffer.
+		reqLen, err := telnetConn.Read(buf)
+		if reqLen > 0 {
+			log.Traceln(buf[:reqLen])
+			telnetConn.Write(buf[:reqLen])
+		}
+		if err != nil {
+			switch err {
+			case io.EOF:
+				// do nothing, as this is expected when a connection is closed by the client
+			default:
+				log.Errorf("%s - %s\n", telnetConn.RemoteAddr(), err.Error())
+			}
+			active = false
+		}
+	}
+}
---
+++ telnet/telnet.go
@@ -0,0 +1,384 @@
+package telnet
+
+import (
+	"bytes"
+	"encoding/binary"
+	log "github.com/sirupsen/logrus"
+	"net"
+)
+
+type ConnectionState int
+
+const (
+	// connection state, used during read operations to process incoming telnet commands
+
+	STATE_DATA ConnectionState = iota
+	STATE_CR
+	STATE_COMMAND
+	STATE_WILL
+	STATE_WONT
+	STATE_DO
+	STATE_DONT
+	STATE_SUBNEG
+	STATE_SUBNEG_IAC
+
+)
+
+const (
+
+	CH_CR 				byte = 13
+	CH_LF 				byte = 10
+	CH_NUL 				byte = 0
+
+	IAC 				byte = 255 // Interpret As Command
+
+	// telnet commands
+
+	CMD_SE 				byte = 240 // End sub negotiation (240)
+	CMD_NOP 			byte = 241 // No operation (241)
+	CMD_DATA 			byte = 242 // Data mark (242)
+	CMD_BREAK 			byte = 243 // Break (243)
+	CMD_IP 				byte = 244 // Interrupt process (244)
+	CMD_ABORT 			byte = 245 // Abort output (245)
+	CMD_AYT 			byte = 246 // Are you there (246)
+	CMD_ERASE_CHAR 		byte = 247 // Erase character (247)
+	CMD_ERASE_LINE 		byte = 248 // Erase line (248)
+	CMD_GA 				byte = 249 // Go ahead (249)
+	CMD_SB 				byte = 250 // Start sub negotiation (250)
+	CMD_WILL			byte = 251
+	CMD_WONT			byte = 252
+	CMD_DO				byte = 253
+	CMD_DONT			byte = 254
+
+	// telnet options
+
+	OPT_BINARY 			byte = 0
+	OPT_ECHO 			byte = 1
+	OPT_SUPPRESS_GA		byte = 3
+	OPT_MSG_SIZE_NEG 	byte = 4
+	OPT_STATUS 			byte = 5
+	OPT_TIMING_MARK 	byte = 6
+	OPT_NAWS			byte = 31
+)
+
+// Telnet specific connection stuff
+
+type Conn struct {
+	net.Conn
+	// used during telnet command processing. See ConnectionState constants above.
+	// We need to store the state across multiple reads. This seemed like a good spot.
+	readState ConnectionState
+	// Bytes received for sub negotation. We also need to store this across multiple reads.
+	subNegBuffer bytes.Buffer
+	// term info we received
+	H int
+	W int
+
+}
+
+
+func (c Conn) SendCommand(cmd byte) (error) {
+	buffer := []byte {
+		IAC,
+		cmd,
+	}
+	_, err := c.Conn.Write(buffer)
+	return err
+}
+
+func (c Conn) SendWill(o byte) (error) {
+	const cmd byte = 251
+	log.Debugf("%s - Send WILL: %d\n", c.RemoteAddr(), o)
+	buffer := []byte {
+		IAC,
+		cmd,
+		o,
+	}
+	_, err := c.Conn.Write(buffer)
+	return err
+}
+
+func (c Conn) SendWont(o byte) (error) {
+	const cmd byte = 252
+	log.Debugf("%s - Send WONT: %d\n", c.RemoteAddr(), o)
+	buffer := []byte {
+		IAC,
+		cmd,
+		o,
+	}
+	_, err := c.Conn.Write(buffer)
+	return err
+}
+
+func (c Conn) SendDo(o byte) (error) {
+	const cmd byte = 253
+	log.Debugf("%s - Send DO: %d\n", c.RemoteAddr(), o)
+	buffer := []byte {
+		IAC,
+		cmd,
+		o,
+	}
+	_, err := c.Conn.Write(buffer)
+	return err
+}
+
+func (c Conn) SendDont(o byte) (error) {
+	const cmd byte = 254
+	log.Debugf("%s - Send DONT: %d\n",c.RemoteAddr(), o)
+	buffer := []byte {
+		IAC,
+		cmd,
+		o,
+	}
+	_, err := c.Conn.Write(buffer)
+	return err
+}
+
+// let's make it compatible with a standard io writer.
+
+// I have no idea if this code works ok under all circumstances (sudden disconnects etc..). Only time will tell.
+// IAC needs to be escaped (=duplicated), otherwise telnet client thinks we are sending a telnet command.
+func (c Conn) Write(data []byte) (totalWritten int, err error) {
+	for len(data) > 0 {
+		var currentWritten int
+		index := bytes.IndexByte(data, IAC)
+		if index==-1 {
+			currentWritten, err = c.Conn.Write(data)
+			totalWritten += currentWritten
+			break
+		} else {
+			// write everything before the IAC byte
+			currentWritten, err = c.Conn.Write(data[:index])
+			totalWritten += currentWritten
+			if err!=nil {
+				log.Errorln(err.Error())
+				break
+			}
+			// write double IAC for escaping purposes
+			currentWritten, err = c.Conn.Write([]byte {IAC, IAC})
+			// not sure if we should account for the fact that more data is written than the original buffer
+			// (because of the IAC doubling). TO-DO: Investigate later.
+			totalWritten += currentWritten
+			if err!=nil {
+				log.Errorln(err.Error())
+				break
+			}
+			// Repeat all this for the rest of the buffer
+			data = data[index+1:]
+		}
+	}
+	return
+}
+
+// Here we read data, and process any telnet command if they are encountered.
+// Escaped IAC bytes (= double IAC byte) should be unescaped here as well.
+func (c Conn) Read(data []byte) (int, error) {
+	destIndex := 0
+	buffer := make([]byte, len(data)) // make a new buffer for reading data, same size as original one
+	tempRead, err := c.Conn.Read(buffer)
+	// process all bytes that have been read, even if an error occured.
+	// This seems to be a recommended approach:
+	//
+	// https://golang.org/pkg/io/
+	//
+	// Callers should always process the n > 0 bytes returned before considering the error err.
+	// Doing so correctly handles I/O errors that happen after reading some bytes and also both of the allowed EOF behaviors.
+	//
+	if tempRead > 0 {
+		for _, element := range buffer[:tempRead] {
+			log.Tracef("%s - Received byte: %d\n", c.RemoteAddr(), element)
+			switch c.readState {
+			case STATE_DATA:
+				switch element {
+				case IAC:
+					log.Tracef("%s - State changed to STATE_COMMAND\n", c.RemoteAddr())
+					c.readState = STATE_COMMAND
+				case CH_CR:
+					c.readState = STATE_CR
+					data[destIndex] = element
+					destIndex++
+					log.Tracef("%s - State changed to STATE_CR\n", c.RemoteAddr())
+				default:
+					// not IAC or CR, so it's data
+					data[destIndex] = element
+					destIndex++
+				}
+
+			case STATE_CR:
+				// only for conversion from CR/LF or CR/NUL to CR. We probably shouldn't do this if telnet is in binary mode.
+				// TO-DO implement binary mode support :-D
+				switch(element) {
+				case CH_LF, CH_NUL:
+					// Do nothing. just ignore this byte.
+				default:
+					// Add to buffer, as it's just normal data
+					data[destIndex] = element
+					destIndex++
+
+				}
+				// switch back to normal data state
+				log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+				c.readState = STATE_DATA
+
+			case STATE_COMMAND:
+				if element==IAC {
+					// this is en escaped byte 255, so just consider it as data
+					data[destIndex] = element
+					destIndex++
+					log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+					c.readState = STATE_DATA
+				} else {
+					// it's a telnet command. Set our processor to the correct state before processing the next byte
+					switch element {
+					case CMD_WILL:
+						log.Tracef("%s - State changed to STATE_WILL\n", c.RemoteAddr())
+						c.readState = STATE_WILL
+					case CMD_DO:
+						log.Tracef("%s - State changed to STATE_DO\n", c.RemoteAddr())
+						c.readState = STATE_DO
+					case CMD_WONT:
+						log.Tracef("%s - State changed to STATE_WONT\n", c.RemoteAddr())
+						c.readState = STATE_WONT
+					case CMD_DONT:
+						log.Tracef("%s - State changed to STATE_DONT\n", c.RemoteAddr())
+						c.readState = STATE_DONT
+					case CMD_SB:
+						log.Tracef("%s - State changed to STATE_SUBNEG\n", c.RemoteAddr())
+						c.readState = STATE_SUBNEG
+						// start of a new subnegotation, so let's reset the buffer
+						c.subNegBuffer.Reset()
+					default:
+						log.Debugf("%s - Received telnet command: %d\n", c.RemoteAddr(), element)
+						log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+						c.readState = STATE_DATA
+					}
+				}
+			case STATE_WILL:
+				log.Debugf("%s - Received WILL: %d\n", c.RemoteAddr(), element)
+				log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+				c.optionHandler(CMD_WILL, element)
+				c.readState = STATE_DATA
+
+			case STATE_WONT:
+				log.Debugf("%s - Received WONT: %d\n", c.RemoteAddr(), element)
+				log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+				c.optionHandler(CMD_WONT, element)
+				c.readState = STATE_DATA
+
+			case STATE_DO:
+				log.Debugf("%s - Received DO: %d\n", c.RemoteAddr(), element)
+				log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+				c.optionHandler(CMD_DO, element)
+				c.readState = STATE_DATA
+
+			case STATE_DONT:
+				log.Debugf("%s - Received DONT: %d\n", c.RemoteAddr(), element)
+				log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+				c.optionHandler(CMD_DONT, element)
+				c.readState = STATE_DATA
+
+			// here we do all the subnegotation handling. This is a bit messy...
+
+			case STATE_SUBNEG:
+				switch element {
+				case IAC:
+					log.Tracef("%s - State changed to STATE_SUBNEG_IAC\n", c.RemoteAddr())
+					c.readState = STATE_SUBNEG_IAC
+				default:
+					// default action is to add data to the buffer
+					c.subNegBuffer.Grow(1)
+					err := c.subNegBuffer.WriteByte(element)
+					if err!=nil {
+						log.Errorln(err.Error())
+					}
+
+				}
+			case STATE_SUBNEG_IAC:
+				switch element {
+				case IAC:
+					// it was an escaped IAC, so add it to the subneg buffer
+					c.subNegBuffer.Grow(1)
+					err := c.subNegBuffer.WriteByte(element)
+					if err!=nil {
+						log.Errorln(err.Error())
+					}
+					log.Tracef("%s - State changed to STATE_SUBNEG\n", c.RemoteAddr())
+					c.readState = STATE_SUBNEG
+
+				case CMD_SE:
+					// received end of subnegotation. Call handler and move back to data mode
+					c.subNegHandler()
+					log.Tracef("%s - State changed to STATE_DATA\n", c.RemoteAddr())
+					c.readState = STATE_DATA
+				}
+
+			}
+		}
+
+	}
+	return destIndex, err
+}
+
+// create and initialize telnet connection object
+func NewConnection(c net.Conn) (*Conn) {
+	conn := Conn {
+		Conn: c,
+	}
+	// set telnet parameters, this should ensure the connection is in character-mode, and echo'ing is done by the server.
+	// We want to be in control of all echo-ing.
+	err := conn.SendWill(OPT_SUPPRESS_GA)
+	if err!=nil {
+		log.Errorln(err.Error())
+	}
+	err = conn.SendWill(OPT_ECHO)
+	if err!=nil {
+		log.Errorln(err.Error())
+	}
+	err = conn.SendDo(OPT_NAWS)
+	if err!=nil {
+		log.Errorln(err.Error())
+	}
+
+	// set terminal size to sane default
+	conn.H = 25
+	conn.W = 80
+
+	return &conn
+}
+
+// private functions
+
+func (c Conn) subNegHandler() {
+	data := c.subNegBuffer.Bytes()
+	log.Traceln(data)
+
+	if len(data) > 0 {
+		option := data[0]
+		switch option {
+		case OPT_NAWS:
+			if len(data) != 5 {
+				log.Errorf("%s - Incorrect amount of parameters for NAWS subnegotiation.\n", c.RemoteAddr())
+				break
+			}
+			w := binary.BigEndian.Uint16(data[1:3])
+			h := binary.BigEndian.Uint16(data[3:5])
+			// only update if size is bigger than zero. SyncTerm seems to send 0's as window size values.
+			if w > 0 {
+				c.W = int(w)
+			}
+			if h > 0 {
+				c.H = int(h)
+			}
+			log.Debugf("%s - terminal size update received (w=%d, h=%d)\n", c.RemoteAddr(), w, h)
+		default:
+			log.Debugf("%s - Unknown subnegotation received (%d). Ignoring.\n", c.RemoteAddr(), option)
+		}
+	}
+	// always reset the buffer after we handled the subnegotiation
+	c.subNegBuffer.Reset()
+
+}
+
+func (c Conn) optionHandler(command byte, option byte) {
+	
+}
